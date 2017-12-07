de0b77cf908b316fabb44eb56e435afb0d88f707
Arthur De Araujo <wafflez180@yahoo.com>
2017-12-07 18:41:49 -0500

    Added day 7 solutions

---
+++ AdventDay7/AdventDay7/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AdventDay7
+//
+//  Created by Arthur De Araujo on 12/7/17.
+//  Copyright © 2017 Arthur De Araujo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
